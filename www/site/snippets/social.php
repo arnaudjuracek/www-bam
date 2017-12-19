2c6fad95cce986669142d351f418f98c3bc28dec
RNO <arnaud.juracek@gmail.com>
2017-12-19 18:17:54 +0100

    Add instagram to social links

--- www/site/snippets/social.php
+++ www/site/snippets/social.php
@@ -4,19 +4,37 @@
       <?php snippet('icons/contact') ?>
     </a>
   </li>
-  <li class="social-link">
-    <a href="<?php echo $site->twitter()->url() ?>">
-      <?php snippet('icons/twitter') ?>
-    </a>
-  </li>
-  <li class="social-link">
-    <a href="<?php echo $site->facebook()->url() ?>">
-      <?php snippet('icons/facebook') ?>
-    </a>
-  </li>
-  <li class="social-link">
-    <a href="<?php echo $site->linkedin()->url() ?>">
-      <?php snippet('icons/linkedin') ?>
-    </a>
-  </li>
+
+  <?php if ($site->twitter()->isNotEmpty()): ?>
+    <li class="social-link">
+      <a href="<?php echo $site->twitter()->url() ?>">
+        <?php snippet('icons/twitter') ?>
+      </a>
+    </li>
+  <?php endif ?>
+
+  <?php if ($site->instagram()->isNotEmpty()): ?>
+    <li class="social-link">
+      <a href="<?php echo $site->instagram()->url() ?>">
+        <?php snippet('icons/instagram') ?>
+      </a>
+    </li>
+  <?php endif ?>
+
+  <?php if ($site->facebook()->isNotEmpty()): ?>
+    <li class="social-link">
+      <a href="<?php echo $site->facebook()->url() ?>">
+        <?php snippet('icons/facebook') ?>
+      </a>
+    </li>
+  <?php endif ?>
+
+  <?php if ($site->linkedin()->isNotEmpty()): ?>
+    <li class="social-link">
+      <a href="<?php echo $site->linkedin()->url() ?>">
+        <?php snippet('icons/linkedin') ?>
+      </a>
+    </li>
+  <?php endif ?>
+
 </ul>
