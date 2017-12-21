arnaudjuracek/www-bam

commit 71e21437b5c95ef344ce2ead83c521909f3a7531

Add no-click opts to image tag

src/index.js
@@ -17,7 +17,7 @@ window.addEventListener('load', blazy.revalidate)
 
 Gallery({ class: 'gallery' })
 
-Zoom(document.querySelectorAll('article img'), {
+Zoom(document.querySelectorAll('figure:not(.no-click) img'), {
   background: '#FFF',
   margin: 10,
   scrollOffset: 1

commit ad2dff889b4eb6f63c4b26be2678c7fd6df50854

Prevent non-article image to be previewed
Fix bug introduced by 71e2143

src/index.js
@@ -17,7 +17,7 @@ window.addEventListener('load', blazy.revalidate)
 
 Gallery({ class: 'gallery' })
 
-Zoom(document.querySelectorAll('figure:not(.no-click) img'), {
+Zoom(document.querySelectorAll('article figure:not(.no-click) img'), {
   background: '#FFF',
   margin: 10,
   scrollOffset: 1