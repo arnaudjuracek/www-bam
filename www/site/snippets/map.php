4e7bbb68d61676955b634dbcf670a651ee58c1ef
RNO <arnaud.juracek@gmail.com>
2018-01-21 19:23:30 +0100

    Change map from gmap to osm
    See #7

--- www/site/snippets/map.php
+++ www/site/snippets/map.php
@@ -1,8 +1,8 @@
 <iframe
   width="100%"
   height="600"
-  frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key=<?php echo c::get('google-map.api-key') ?>
-    &q=<?php echo escape::url($page->address()) ?>&language=FR" allowfullscreen>
+  frameborder="0"
+  scrolling="no"
+  style="border:0"
+  src="http://www.openstreetmap.org/export/embed.html?bbox=2.363369464874268%2C48.85992931145876%2C2.373411655426026%2C48.86470770395339&amp;layer=transportmap&amp;marker=48.86231856473312%2C2.3683905601501465">
 </iframe>
-
