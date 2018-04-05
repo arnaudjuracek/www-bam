f6410eaf7c1aa48bac0baa75f9a424962d647e51
RNO <arnaud.juracek@gmail.com>
2018-04-05 10:00:49 +0200

    Hotfix google analytics

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <?php snippet('header.metas') ?>
-  <?php echo liveCSS('assets/bundle.css') ?>
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-42831572-1"></script>
   <script>
     window.dataLayer = window.dataLayer || []
-    function gtag(){dataLayer.push(arguments);
+    function gtag () { dataLayer.push(arguments); }
     gtag('js', new Date())
     gtag('config', 'UA-42831572-1')
   </script>
+  <?php snippet('header.metas') ?>
+  <?php echo liveCSS('assets/bundle.css') ?>
 </head>
 <body
   data-category="<?php echo getTopLevelPage($page)->uid() ?>"
