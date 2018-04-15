fac43427e56110eab62d36eaa9f7d52935d5bb49
RNO <arnaud.juracek@gmail.com>
2018-04-15 20:18:38 +0200

    Allow panel modification of the OSMap
    Fix #7

--- www/site/snippets/map.php
+++ www/site/snippets/map.php
@@ -4,5 +4,5 @@
   frameborder="0"
   scrolling="no"
   style="border:0"
-  src="https://www.openstreetmap.org/export/embed.html?bbox=2.363369464874268%2C48.85992931145876%2C2.373411655426026%2C48.86470770395339&amp;layer=transportmap&amp;marker=48.86231856473312%2C2.3683905601501465">
+  src="<?php echo $page->mapsrc()->value() ?>">
 </iframe>
