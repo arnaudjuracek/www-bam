1db7e5870a59e49096222db37998a61a9c7045fa
RNO <arnaud.juracek@gmail.com>
2018-07-31 14:37:27 +0200

    Replace ganalytics by matmoto

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-42831572-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || []
-    function gtag () { dataLayer.push(arguments); }
-    gtag('js', new Date())
-    gtag('config', 'UA-42831572-1')
-  </script>
   <?php snippet('header.metas') ?>
   <?php echo liveCSS('assets/bundle.css') ?>
+  <!-- Matomo -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//collectifbam.fr/piwik/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Matomo Code -->
 </head>
 <body
   data-category="<?php echo getTopLevelPage($page)->uid() ?>"
