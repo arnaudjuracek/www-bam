464d5d6fccbfc8f4c73422deab3f76ab898020f5
RNO <arnaud.juracek@gmail.com>
2019-12-05 18:18:18 +0100

    Revamp splashcreen, update logos

--- www/site/templates/home.php
+++ www/site/templates/home.php
@@ -3,14 +3,20 @@
 <section class="splashscreen">
   <?php snippet('menu') ?>
 
-  <div class="splashscreen-logo">
-    <a href="<?php echo page('collectif-bam')->url() ?>">
-      <img src="<?php echo $kirby->urls()->assets() ?>/images/logo-full.svg">
-    </a>
-  </div>
+  <div class="splashscreen-wrapper">
+    <div class="splashscreen-logo">
+      <a href="<?php echo page('collectif-bam')->url() ?>">
+        <img src="<?php echo $kirby->urls()->assets() ?>/images/logo-full.svg">
+      </a>
+    </div>
 
-  <div class="splashscreen-baseline">
-    <?php echo $page->text()->kirbytext() ?>
+    <div class="splashscreen-baseline">
+      <?php echo $page->text()->html() ?>
+    </div>
+
+    <div class="splashscreen-hometext">
+      <?php echo $site->hometext()->kirbytext() ?>
+    </div>
   </div>
 
   <footer class="splashscreen-footer">
